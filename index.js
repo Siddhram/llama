a2be42c5762410b317fbf26fbb7915c8ca0bf67a
Siddharam Sutar <147894262+Siddhram@users.noreply.github.com>
2025-01-19 02:36:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const cors=require('cors');
 app.use(express.json());
 //llama
+app.use(cors());
 const Groq = require('groq-sdk');
 
 const groq = new Groq({
